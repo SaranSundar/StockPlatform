aa4f547de0ef48a5f86100e2495934e912aab154
Saran Sundararajan <saransszg@gmail.com>
2019-03-14 18:08:34 -0500

    setup

--- Python/stock_scraper.py
+++ Python/stock_scraper.py
@@ -307,16 +307,16 @@ def set2():
     file_name = "scraped_stocks2.bin"
     search_amount = 10  # Arbitrarily large value to scrape all available stocks
     format_type = 'pandas'  # Can also be pandas
-    (data_stocks, filters) = start_scraping(should_download=False, file_name=file_name, search_amount=search_amount,
+    (data_stocks, filters) = start_scraping(should_download=True, file_name=file_name, search_amount=search_amount,
                                             format_type=format_type)
     return data_stocks, filters
 
 
 def main():
-    (data_stocks1, filters1) = set1()
+    # (data_stocks1, filters1) = set1()
     (data_stocks2, filters2) = set2()
-    # console_app(data_stocks, filters)
-    json_to_df(data_stocks1, data_stocks2)
+    console_app(data_stocks2, filters2)
+    # json_to_df(data_stocks1, data_stocks2)
     print("-------EXODIUS v1.0-------")
 
 
